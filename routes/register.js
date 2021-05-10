046d113d39a9570e5128f07f816723e78270f503
cls688 <1536463948@qq.com>
2021-05-10 10:04:48 +0800

    post请求解析

--- routes/register.js
+++ routes/register.js
@@ -3,11 +3,22 @@ var router = express.Router();
 
 /* GET register listing. */
 router.get('/', function(req, res, next) {
-  res.send('这是注册界面Get');
+  let message = req.query;
+  // console.log(message)
+  let username = message.newusername;
+  let password = message.newpassword;
+  let feedBack = username+password;
+  res.send(feedBack);
 });
 
 router.post('/', function(req, res, next) {
-    res.send('这是注册界面Post');
-  });
+  // console.log(req.body)
+  let newUser = req.body
+  let username = newUser.params.newusername
+  let password = newUser.params.newpassword
+  console.log(username)
+  console.log(password)
+  res.send('这是注册界面Post');
+});
 
 module.exports = router;
